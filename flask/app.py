d087e2c6b83cd03e1874968605d6f5c976b9fa2a
Charles Leifer <coleifer@gmail.com>
2010-07-04 17:30:19 -0500

    Adding support for flask site generation

---
+++ flask/app.py
@@ -0,0 +1,61 @@
+import sqlite3
+from contextlib import closing
+from flask import Flask, request, session, g, redirect, url_for, \
+        abort, render_template, flash
+
+# config
+DATABASE = '{{ site_name }}.db'
+DEBUG = True
+SECRET_KEY = '{{ secret_key }}'
+
+USERNAME = '{{ user }}'
+PASSWORD = 'password'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('homepage'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('homepage'))
+
+
+if __name__ == '__main__':
+    app.run()
