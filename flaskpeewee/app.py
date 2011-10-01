234470949686fa22b514eb84de477616e6b748dc
Charles Leifer <coleifer@gmail.com>
2011-09-30 21:59:39 -0500

    Small cleanups related to API changes in skew -- fixing up flaskpeewee

--- flaskpeewee/app.py
+++ flaskpeewee/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
 from flaskext.db import Database
+from skew.queue import Invoker
+
+from config import Configuration, QueueConfiguration
 
 
 app = Flask(__name__)
-app.config.from_object('config.Configuration')
+app.config.from_object(Configuration)
 
 db = Database(app)
+
+invoker = Invoker(QueueConfiguration.QUEUE)
--- flaskpeewee/config.py
+++ flaskpeewee/config.py
@@ -1,11 +1,26 @@
+import logging
 import os
+
+from skew.backends.redis_backend import RedisQueue
+from skew.bin.config import BaseConfiguration
+
 
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
 
+
 class Configuration(object):
     DATABASE = {
-        'name': '{{ site_name }}.db',
+        'name': '{{ project_root }}{{ site_name }}.db',
         'engine': 'peewee.SqliteDatabase',
     }
     DEBUG = {% if staging %}True{% else %}False{% endif %}
     SECRET_KEY = '{{ secret_key }}'
+    APP_ROOT = APP_ROOT
+    MEDIA_ROOT = '%s/media/' % (APP_ROOT)
+    MEDIA_URL = '/media'
+
+
+class QueueConfiguration(BaseConfiguration):
+    QUEUE = RedisQueue('{{ site_name }}', 'localhost:6379:0')
+    LOGFILE = '{{ logs }}skew.log'
+    LOGLEVEL = logging.DEBUG
